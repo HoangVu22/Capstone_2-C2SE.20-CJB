69d51bdeb94bbf3925436e358e2f963e6f3faf9e
DungVT27 <dungvt@runsystem.net>
2023-03-27 10:06:35 +0700

    Fix login api

--- BackEnd/app/Models/UserProfile.php
+++ BackEnd/app/Models/UserProfile.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class UserProfile extends Model
 {
     use HasFactory;
 
     protected $table = 'user_profiles';
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
