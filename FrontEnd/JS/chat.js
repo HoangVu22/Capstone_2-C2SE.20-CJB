8f3cc62f2242c7cf5a8a1d35822deb49ae864049
VietAnh188 <nguyenthuyhang705@gmail.com>
2023-05-03 22:10:58 +0700

    socket init

--- FrontEnd/JS/chat.js
+++ FrontEnd/JS/chat.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
-    $("#action_menu_btn").click(function () {
-      $(".action_menu").toggle();
-    });
+  $("#action_menu_btn").click(function () {
+    $(".action_menu").toggle();
   });
+});
+
+const socket = io("http://localhost:3002/chat");
+const messageInput = document.querySelector(".type_msg");
+const sendButton = document.querySelector(".send_btn");
+const messages = document.querySelector(".msg_card_body");
+
+sendButton.onclick = () => {
+  if (messageInput.value) {
+    const targetMessage = `
+    <div class="d-flex justify-content-end mb-4">
+        <div class="msg_cotainer_send">
+            ${messageInput.value}
+            <span class="msg_time_send">9:10 AM, Today</span>
+        </div>
+    </div>`;
+    messages.innerHTML += targetMessage;
+    socket.emit("send_message", messageInput.value);
+    messageInput.value = "";
+  }
+};
+
+socket.on("receive_message", (message) => {
+  const targetMessage = `<div class="d-flex justify-content-start mb-4">
+      <div class="img_cont_msg">
+        <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
+      </div>
+      <div class="msg_cotainer">
+        ${message}
+        <span class="msg_time">8:40 AM, Today</span>
+      </div>
+    </div>`;
+  messages.innerHTML += targetMessage;
+});
--- FrontEnd/JS/login-register.js
+++ FrontEnd/JS/login-register.js
@@ -1,80 +1,80 @@
-const registerButton1 = document.getElementById('register')
-const loginButton1 = document.getElementById('login')
-const loginButton = document.getElementById('login-1')
-const container = document.getElementById('container')
-const names = document.getElementsByClassName('header-name1');
+const registerButton1 = document.getElementById("register");
+const loginButton1 = document.getElementById("login");
+const loginButton = document.getElementById("login-1");
+const container = document.getElementById("container");
+const names = document.getElementsByClassName("header-name1");
 const $ = document.querySelector.bind(document);
 
-registerButton1.addEventListener('click', () => {
-    container.classList.add('right-panel-active')
-})
+registerButton1.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
 
-loginButton1.addEventListener('click', () => {
-    container.classList.remove('right-panel-active')
-})
+loginButton1.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
 
-loginButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    const inputs = document.querySelectorAll('input.form-control');
-    const requestValues = {};
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputs = document.querySelectorAll("input.form-control");
+  const requestValues = {};
 
-    inputs.forEach(item => {
-        requestValues[item.attributes.name.value] = item.value;
-    });
-    console.log(requestValues);
-    fetch('http://127.0.0.1:8000/api/auth/login', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestValues)
+  inputs.forEach((item) => {
+    requestValues[item.attributes.name.value] = item.value;
+  });
+  console.log(requestValues);
+  fetch("http://127.0.0.1:8000/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestValues),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === 200) {
+        window.localStorage.setItem("login", JSON.stringify(data));
+        console.log(data.user_info.user_role);
+        window.localStorage.setItem(
+          "id",
+          JSON.stringify(data.user_info.user_profile[0].user_id)
+        );
+        window.localStorage.getItem("login");
+        console.log(data);
+        window.location.href = "http://localhost:3000/home.html";
+      } else {
+        alert(data.msg);
+      }
     })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 200) {
-                window.localStorage.setItem("login", JSON.stringify(data));
-                console.log(data.user_info.user_role);
-                window.localStorage.setItem("id", JSON.stringify(data.user_info.user_profile[0].user_id))
-                window.localStorage.getItem("login");
-                console.log(data);
-                window.location.href = 'http://localhost:3000/FrontEnd/HTML/home.html';
-            } else {
-                alert(data.msg)
-            }
-        })
-        .catch(error => console.log(error))
-})
+    .catch((error) => console.log(error));
+});
 
 // --------------------------------------------------------------------
 
-
-
-const registerButton = $('.resgister-1');
+const registerButton = $(".resgister-1");
 
 registerButton.onclick = (e) => {
-    e.preventDefault();
-    const inputs = document.querySelectorAll('input.form-input')
-    const requestValues = {}
+  e.preventDefault();
+  const inputs = document.querySelectorAll("input.form-input");
+  const requestValues = {};
 
-    inputs.forEach(item => {
-        requestValues[item.attributes.name.value] = item.value;
-    })
-    fetch('http://127.0.0.1:8000/api/auth/userRegister', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestValues)
-    })
-        .then(response => response.json())
-        .then(
-            data => {
-                if (data.status === 200) {
-                    alert("success......");
-                    window.location.href = 'http://127.0.0.1:5500/CAPSTONE2/FrontEnd/HTML/home.html'
-                } else {
-                    alert(data.message)
-                }
-            }
-        )
-}
+  inputs.forEach((item) => {
+    requestValues[item.attributes.name.value] = item.value;
+  });
+  fetch("http://127.0.0.1:8000/api/auth/userRegister", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestValues),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === 200) {
+        alert("success......");
+        window.location.href =
+          "http://127.0.0.1:5500/CAPSTONE2/FrontEnd/HTML/home.html";
+      } else {
+        alert(data.message);
+      }
+    });
+};
