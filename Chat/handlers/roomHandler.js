c2bcf9a49cc75a0a54775c7151c3433c2be623e2
VietAnh188 <nguyenthuyhang705@gmail.com>
2023-05-10 00:34:26 +0700

    confirm notification

--- Chat/handlers/roomHandler.js
+++ Chat/handlers/roomHandler.js
@@ -10,6 +10,38 @@ module.exports = (io) => {
 
     connectUsers[userId] = socket;
 
+    const existing = (id) => {
+      if (connectUsers[id]) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    socket.on("verify-join-room", async ({ id, value }) => {
+      await members.update(
+        {
+          is_confirm: value,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      const member = await members.findByPk(id);
+      const room = await rooms.findByPk(member.room_id);
+      // create new notification on database
+      if (existing(member.user_id)) {
+        connectUsers[member.user_id].emit("verify-join-room", {
+          message: value
+            ? "Bạn đã được xác nhận tham gia vào nhóm"
+            : "Yêu cầu tham gia nhóm của bạn bị từ chối",
+          room: room.name,
+        });
+      }
+    });
+
     socket.on("join-room", async ({ roomId, joiner }) => {
       const room = await rooms.findByPk(roomId);
       const member = await members.findOne({
@@ -19,12 +51,17 @@ module.exports = (io) => {
           is_confirm: false,
         },
       });
-      const user = await users.findByPk(joiner);
-      connectUsers[room.room_owner].emit("join-room", {
-        who: user.name,
-        message: `đang muốn tham gia vào phòng của bạn`,
-        verifyId: member.id,
-      });
+      if (member) {
+        const user = await users.findByPk(joiner);
+        // create new notification on database
+        if (connectUsers[room.room_owner]) {
+          connectUsers[room.room_owner].emit("join-room", {
+            who: user.name,
+            message: `đang muốn tham gia vào phòng của bạn`,
+            verifyId: member.id,
+          });
+        }
+      }
     });
 
     socket.on("disconnect", () => {
