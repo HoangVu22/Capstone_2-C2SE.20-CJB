b8538b3bce93d17f4ac6ace39c518e322d3dfb1d
DungVT27 <dungvt@runsystem.net>
2023-05-09 21:19:41 +0700

    Fix friends

--- BackEnd/app/Http/Controllers/FriendController.php
+++ BackEnd/app/Http/Controllers/FriendController.php
@@ -24,11 +24,7 @@ class FriendController extends Controller
             'user_id' => $request->user_id,
             'friend_id' => $request->friend_id,
         ]);
-        Friend::create([
-            'user_id' => $request->friend_id,
-            'friend_id' => $request->user_id,
-        ]);
-
+        
         return response()->json(['msg' => 'Make friend thành công']);
     }
 
@@ -56,10 +52,6 @@ class FriendController extends Controller
         Friend::where([
             'user_id' => $request->user_id,
             'friend_id' => $request->friend_id,
-        ])->delete();
-        Friend::where([
-            'user_id' => $request->friend_id,
-            'friend_id' => $request->user_id,
         ])->delete();
 
         return response()->json(['msg' => 'Unfriend thành công']);
