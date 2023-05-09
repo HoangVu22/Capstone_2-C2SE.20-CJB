06d932311ed4062c339c7f2d102207dd3c96bdec
DungVT27 <dungvt@runsystem.net>
2023-05-09 22:38:07 +0700

    Fix make friend api

--- BackEnd/app/Http/Controllers/FriendController.php
+++ BackEnd/app/Http/Controllers/FriendController.php
@@ -20,12 +20,31 @@ class FriendController extends Controller
      */
     public function store(Request $request)
     {
-        Friend::create([
-            'user_id' => $request->user_id,
-            'friend_id' => $request->friend_id,
-        ]);
-        
-        return response()->json(['msg' => 'Make friend thành công']);
+        if($request->user_id == $request->friend_id){
+            return response()->json([
+                'msg' => 'Không thể follow, user_id = friend_id',
+                'status' => 403,
+            ]);
+        }
+        $hasFollow = Friend::where('user_id', $request->user_id)->where('friend_id', $request->friend_id)->get()->toArray();
+
+        if(count($hasFollow) > 0){
+            return response()->json([
+                'msg' => 'Bạn đã follow người này rồi',
+                'status' => 403,
+            ]);
+        }
+        else{
+            Friend::create([
+                'user_id' => $request->user_id,
+                'friend_id' => $request->friend_id,
+            ]);
+            
+            return response()->json([
+                'msg' => 'Make friend thành công',
+                'status' => 200
+            ]);
+        }
     }
 
     /**
