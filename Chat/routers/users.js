fe2c6774bdcf414e01b2570e260552c597cd6db9
nnhang04 <99669638+nnhang04@users.noreply.github.com>
2023-05-10 09:33:44 +0700

    Delete Chat directory

--- Chat/routers/users.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { findById } = require("../controllers/users");
-
-const router = Router();
-
-router.get("/:id", findById);
-
-module.exports = router;
